36b9de75025d689ff813b5223520c4769f4208c6
Gea-Suan Lin <gslin@gslin.com>
2026-03-30 02:38:16 +0800

    feat: open config page for first install.

--- src/background.js
+++ src/background.js
@@ -1,5 +1,11 @@
 "use strict";
 
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.runtime.openOptionsPage();
+  }
+});
+
 chrome.action.onClicked.addListener(() => {
   chrome.runtime.openOptionsPage();
 });
